3deadeed3b9cb156e65185b68ebb554da4a44a2a
i-zakiro <90312517+i-zakiro@users.noreply.github.com>
2023-02-24 20:39:48 +0600

    <BrowserRouter basename="/react-typescript-deploy-test">

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 )
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/react-typescript-deploy-test">
     <ModalState>
       <App />
     </ModalState>
